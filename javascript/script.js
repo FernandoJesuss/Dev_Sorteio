dfa5f188a94fafca70813cfc78f643ec65ccd1ec
fernandoJesuss <fernando_ffernando@hotmail.com>
2024-09-02 19:02:02 -0300

    nova funcionalidade

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,19 @@
-function generateNumber(){
+function generateNumber() {
 
-const min = Math.ceil(document.querySelector(".inpu-min").value)
-const max = Math.floor ( document.querySelector(".input-max").value)
+    const min = Math.ceil(document.querySelector(".inpu-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
 
-const result = Math.floor(Math.random() * (max - min +1)) +min;
+    if (min >= max) {
+        
+        alert ("O valor minimo tem que ser MENOR que o valor máximo")
 
-document.getElementById("result").innerText = "Resultado: " + result;
+    } else {
+
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        document.getElementById("result").innerText = "Resultado: " + result;
+
+    }
 
 }
 
